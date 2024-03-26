e26f52ea37d2e7bbde061917430fe2c6bdf748a7
AbdulhameedAjibola <105476350+AbdulhameedAjibola@users.noreply.github.com>
2024-03-26 08:27:43 +0400

    Update Lessons.js with second lesson

--- lessons.js
+++ lessons.js
@@ -8,5 +8,15 @@ let lessons = [
         "description": "This is an extra lesson session for students who wish to improve their Table tennis skills",
         "availability": 10
     },
-    
+    {
+        "id": 1002,
+        "image": "images/pet2.jpg",
+        "subject": "BBC 224",
+        "price": 150.00,
+        "location": "Computer Science Building (CSB) Room 306",
+        "description": "This lesson is for business students who wish to learn how they can leverage tech in their applications",
+        "availability": 10
+        
+    },
+   
 ]
