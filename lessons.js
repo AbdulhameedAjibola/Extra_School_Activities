d829ff19dbdabe268c8f57879125379c7e6311e4
AbdulhameedAjibola <105476350+AbdulhameedAjibola@users.noreply.github.com>
2024-03-26 08:28:19 +0400

    Added 2 more lessons to lessons.js

--- lessons.js
+++ lessons.js
@@ -18,5 +18,32 @@ let lessons = [
         "availability": 10
         
     },
-   
+    {
+        "id": 1003,
+        "image": "images/pet2.jpg",
+        "subject": "Basketball 101",
+        "price": 45.00,
+        "location": "Sports Centre GK17",
+        "description": "This is an extra lesson session for students who wish to improve their Basketball playing skills",
+        "availability": 10
+    },
+    {
+        "id": 1004,
+        "image": "images/pet2.jpg",
+        "subject": "Volleyball 101",
+        "price": 15.00,
+        "location": "Outdoor Volleyball Coutt",
+        "description": "Cone join us, in a fun filled learning environment and improve your volleyball skills!",
+        "availability": 10
+    },
+    {
+        "id": 1005,
+        "image": "images/pet2.jpg",
+        "subject": "BIO 332",
+        "price": 0,
+        "location": "Biology Lab",
+        "description": "This course is for students who wish to improve their biology understanding!!. Stand out anongst your peers!",
+        "availability": 10
+    },
+    
 ]
