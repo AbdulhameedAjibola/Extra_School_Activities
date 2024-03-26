372ed05c94b3ac61f89f0ea54495fc9d360afbb6
AbdulhameedAjibola <105476350+AbdulhameedAjibola@users.noreply.github.com>
2024-03-26 08:27:03 +0400

    Create lessons.js with first lesson

---
+++ lessons.js
@@ -0,0 +1,12 @@
+let lessons = [
+    {
+        "id": 1001,
+        "image": "images/pet2.jpg",
+        "subject": "Tennis 101",
+        "price": 50.00,
+        "location": "Sports Centre GK17",
+        "description": "This is an extra lesson session for students who wish to improve their Table tennis skills",
+        "availability": 10
+    },
+    
+]
